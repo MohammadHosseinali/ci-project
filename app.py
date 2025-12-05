db3ba87627223ac538ab4c4a3e2ff190fe856e6c
Mohammad <mohammad.hoseinali1@gmail.com>
2025-12-05 11:51:46 +0330

    Linting

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, request
-app = Flask(__name__)
 
+app = Flask(__name__)
 db = []
 
-def helper_function(m, n):
+
+def helper_function(m: int, n: int) -> int:
     return m + n
+
 
 def add_to_database(username):
     db.append(username)
 
+
 @app.route('/ping')
 def ping():
-    return {'status':'ok'}
+    return {'status': 'ok'}
 
 
 @app.route('/add', methods=["POST"])
 def add_user():
     username = request.json['username']
     add_to_database(username)
-    return {'status':'ok'}
+    return {'status': 'ok'}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
